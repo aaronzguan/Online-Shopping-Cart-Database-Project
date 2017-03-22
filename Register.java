4f3725f8f8b0088a73593fea969b9990f6e79977
guanzhong1996 <zhong.guan@mail.mcgill.ca>
2017-03-21 23:25:49 -0400

    Zhong update

--- Register.java
+++ Register.java
@@ -13,13 +13,16 @@ public class Register extends JFrame{
 	JTextField bank = new JTextField("Bank");
 	JTextField organization = new JTextField("Organization");
 	JButton submit = new JButton("submit");
+	MainFrame mainFrame = null;
+	Register frame = this;
 	
 	SQL adduser;
 	ResultSet rs;
 	int userid;
 	String sqlcode;
 	
-	public Register(SQL sqlo){
+	public Register(SQL sqlo,MainFrame mainFrame){
+		this.mainFrame = mainFrame;
 		adduser=sqlo;
 		JPanel jpanel = new JPanel();
 		jpanel.setLayout(new GridLayout(6,2));
@@ -34,17 +37,17 @@ public class Register extends JFrame{
 		submit.setPreferredSize(new Dimension(20,40));
 		this.add(submit, BorderLayout.SOUTH);
 	}
-	public static void invoke (SQL sqlo){
-		JFrame register = new Register(sqlo);
+	public static void invoke (SQL sqlo,MainFrame mainFrame){
+		JFrame register = new Register(sqlo,mainFrame);
 		register.setVisible(true);
 		register.setSize(400, 200);
-		register.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		register.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		register.setLocationRelativeTo(null);
 		register.setTitle("Register a new User");
 	}
 	class registerlistener implements ActionListener{
 		public void actionPerformed(ActionEvent e){
-			sqlcode ="";// get the max userid from user table
+			sqlcode ="select max(userid) from users";// get the max userid from user table
 			rs = adduser.QueryExchte(sqlcode);
 			userid = Integer.parseInt(rs.toString()) + 1;
 			
@@ -54,12 +57,22 @@ public class Register extends JFrame{
 			String Expirydate = expirydate.getText();
 			String Bank = bank.getText();
 			String Org = organization.getText();
-			sqlcode = ""; // Insert a new buyer to table user and buyer.
-			if(Name.isEmpty()||Pnum.isEmpty()||Cardnum.isEmpty()||Expirydate.isEmpty()||Bank.isEmpty()||Org.isEmpty())
+			if(Name.trim().isEmpty()||Pnum.trim().isEmpty()||Cardnum.trim().isEmpty()||Expirydate.trim().isEmpty()||Bank.trim().isEmpty()||Org.trim().isEmpty())
 				JOptionPane.showMessageDialog(null, "It is required to fill in every blank","Error",JOptionPane.ERROR_MESSAGE);
 			else{
+				sqlcode = "insert into users values ("+userid+", \'"+Name+"\', \'"+Pnum+"\')"; // Insert a new buyer to table user and buyer.
 				adduser.WriteExcute(sqlcode);
+				sqlcode = "insert into bankcard values ("+Cardnum+", \'"+Expirydate+"\', \'"+Bank+"\')";
+				adduser.WriteExcute(sqlcode);
+				sqlcode = "insert into creitcard values ("+Cardnum+","+userid+", \'"+Org+"\')";
+				adduser.WriteExcute(sqlcode);
+				
+				JOptionPane.showMessageDialog(null, "You have successfully registed","Register Successfully",JOptionPane.OK_OPTION);
 				//Return back the current user id
+				
+				mainFrame.setUserid(userid);
+				mainFrame.setVisible(true);
+				frame.dispose();
 			}
 		}
 		
